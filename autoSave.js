b5ae1cd775a8b032ef1c81e51ef33ad72189ea7f
pnshiralkar <pnshiralkar@gmail.com>
2020-09-19 20:35:13 +0530

    AutoSave recursive save bug fixed

--- autoSave.js
+++ autoSave.js
@@ -20,7 +20,6 @@ class autoSaver {
                     "Authorization": `SessionToken ${this.token}`
                 }
             }
-            console.log(this.config)
         }catch (e) {
             console.error(`AutoSave Error : Convergence login error`)
         }
@@ -44,40 +43,43 @@ class autoSaver {
                 return
             }
         }
-        if (!fs.existsSync(`${process.env.USER_DATA_BASE_PATH}/${projectId}`))
-            fs.mkdirSync(`${process.env.USER_DATA_BASE_PATH}/${projectId}`)
-        this.saveRec(res.data.body.data["tree"], projectId, "root").then()
+        this.saveRec(res.data.body.data["tree"], projectId, `${process.env.USER_DATA_BASE_PATH}/${projectId}`, "root").then()
     }
 
-    saveRec = async (dataTree, projectId, dir) => {
+    saveRec = async (dataTree, projectId, basePath, dir) => {
         const files = dataTree.nodes[dir].children;
+
+        // Create directory if it does not exist
+        if (!fs.existsSync(basePath))
+            fs.mkdirSync(basePath)
+
         files.map(async (file) => {
-            let fileRes;
-            try {
-                fileRes = await axios.get(`${process.env.OT_SERVER_URL}/rest/domains/convergence/default/models/${file}`, this.config)
-            } catch (e) {
-                if(e.response && (e.response.status === 401 || e.response.status === 403)){
-                    await this.generateToken()
-                    try{
-                        fileRes = await axios.get(`${process.env.OT_SERVER_URL}/rest/domains/convergence/default/models/${file}`, this.config)
-                    } catch (e) {
-                        console.error(`AutoSave Error : Convergence generated token not accepted for roomID ${projectId}`)
-                        return;
-                    }
-                } else {
-                    console.error(`AutoSave Error : Convergence API error occurred for roomID ${projectId}`)
-                }
-            }
-            const filename = dataTree.nodes[file].name
-            console.log(`AutoSave : Saved file ${projectId}/${filename}`)
-            // If data has children, it's a folder so call saveRec recursively else save file
-            // TODO: verify recursive working, currently works for all files in root directory
-            if (fileRes.data.body.data.children) {
-                this.saveRec(dataTree, projectId, filename).then()
+            // If node-data has children, it's a directory so call saveRec recursively else save file
+            if (dataTree.nodes[file].children) {
+                this.saveRec(dataTree, projectId, `${basePath}/${dataTree.nodes[file].name}`, file).then()
             } else {
-                fs.writeFile(`${process.env.USER_DATA_BASE_PATH}/${projectId}/${filename}`, fileRes.data.body.data.content + "\n", (err) => {
+                let fileRes;
+                try {
+                    fileRes = await axios.get(`${process.env.OT_SERVER_URL}/rest/domains/convergence/default/models/${file}`, this.config)
+                } catch (e) {
+                    if(e.response && (e.response.status === 401 || e.response.status === 403)){
+                        await this.generateToken()
+                        try{
+                            fileRes = await axios.get(`${process.env.OT_SERVER_URL}/rest/domains/convergence/default/models/${file}`, this.config)
+                        } catch (e) {
+                            console.error(`AutoSave Error : Convergence generated token not accepted for roomID ${projectId}`)
+                            return;
+                        }
+                    } else {
+                        console.error(`AutoSave Error : Convergence API error occurred for roomID ${projectId}`)
+                    }
+                }
+                const filename = dataTree.nodes[file].name
+                fs.writeFile(`${basePath}/${filename}`, fileRes.data.body.data.content + "\n", (err) => {
                     if (err)
                         console.log(err)
+                    else
+                        console.log(`AutoSave : Saved file ${basePath.split(process.env.USER_DATA_BASE_PATH)[1].substr(1)}/${filename}`)
                 })
             }
         })
